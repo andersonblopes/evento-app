e7b0e7c57683d42063c55a71eb76de8d3c2dbd79
andersonblopes <anderson.lopes.2010@gmail.com>
2017-12-11 20:59:50 -0300

    List data of database

--- src/main/java/com/appevento/eventoapp/controller/EventoController.java
+++ src/main/java/com/appevento/eventoapp/controller/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.appevento.eventoapp.model.Evento;
 import com.appevento.eventoapp.repository.EventoRepository;
@@ -19,11 +20,19 @@ public class EventoController {
 		return "evento/form-evento";
 	}
 
-	@RequestMapping(value="/cadastroevento", method= RequestMethod.POST)
+	@RequestMapping(value = "/cadastroevento", method = RequestMethod.POST)
 	public String form(Evento evento) {
-		
+
 		eventoRepository.save(evento);
 		return "redirect:/cadastroevento";
+	}
+
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView modelAndView = new ModelAndView("index");
+		Iterable<Evento> eventos = eventoRepository.findAll();
+		modelAndView.addObject("eventos", eventos);
+		return modelAndView;
 	}
 
 }
